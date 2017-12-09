4220a438143c4484f748fee9621911a25155c26a
ClassicKerobel <josealonsomendoza@hotmail.com>
2017-12-09 17:01:33 -0600

    Now the servos can be controlled by a keypady

--- servo_controller.ino
+++ servo_controller.ino
@@ -1,49 +1,130 @@
+#include <Keypad.h>
 #include <Servo.h>
 
-Servo servo1;  // create servo object to control a servo
-Servo servo2; 
+// rows and columns of keypad
+const byte ROWS = 2;
+const byte COLS = 2;
 
-const int servoButton1 = 2;
-const int servoButton2 = 3;
+// array of all the keys
+char keys[ROWS][COLS] = {
+  {'D', '#'},
+  {'C', '9'}
+};
 
-// actual position of each servo
-int servo1Val = 0;   
-int servo2Val = 0;    
-// states of both servos
-int button1State = 0;
-int button2State = 0;
+// binding pins with each kepad row and column
+byte rowPins[ROWS] = {4, 5};
+byte colPins[COLS] = {2, 3};
 
+// defining the keypad
+Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+// defining servos
+Servo servo0;
+Servo servo1;
+
+// pot that is used to modify speed of servo
 int potpin = 0;
-// speed of the servos
-int servoSpeed;
+int servo_speed;
+
+// min val
+int servos_min_val = 10;
+// max val
+int servos_max_val = 180;
+
+int servo0_pos = servos_min_val;
+int servo1_pos = servos_min_val;
+
+// variable in order to know if the servo is moving upwards or downwards
+// first dimension is each servo and the third one says movement 
+// -1 = going down towards servos_min_Val
+// 0 = maintining position
+// 1 = going upwards to 180
+int servos_moving[2][1] = {
+  {0},
+  {0},
+};
 
 void setup() {
-  servo1.attach(9);  // attaches the servo on pin 9 to the servo object
-  servo2.attach(10);
-  pinMode(servoButton1, INPUT);
-  pinMode(servoButton2, INPUT);
+  servo0.attach(9);
+  servo1.attach(10);
+
+  servo0.write(10);
+  servo1.write(servo1_pos);
+  
+  Serial.begin(9600);
+
 }
+
 
 void loop() {
-  button1State = digitalRead(servoButton1);
-  button2State = digitalRead(servoButton2);
-  servoSpeed = analogRead(potpin);
-  servoSpeed = map(servoSpeed, 0, 1023, 1, 20);
+  servo_speed = analogRead(potpin);
+  servo_speed = map(servo_speed, 0, 1023, 1, 15);
 
-  if (button1State == HIGH && servo1Val < 180){
-    servo1Val++;
+
+  if (servos_moving[0][0] == -1 && servo0_pos > servos_min_val) {
+    servo0_pos--;
+    servo0.write(servo0_pos);
+    delay(servo_speed);
   }
-  else if (servo1Val > 0) {
-    servo1Val--;
+  if (servos_moving[0][0] == 1 && servo0_pos < servos_max_val) {
+    servo0_pos++;
+    servo0.write(servo0_pos);
+    delay(servo_speed);
   }
-  if (button2State == HIGH && servo2Val < 180) {
-    servo2Val++;
+
+  if (servos_moving[1][0] == -1 && servo1_pos > servos_min_val) {
+    servo1_pos--;
+    servo1.write(servo1_pos);
+    delay(servo_speed);
   }
-  else if (servo2Val > 0) {
-    servo2Val--;
+  if (servos_moving[1][0] == 1 && servo1_pos < servos_max_val) {
+    servo1_pos++;
+    servo1.write(servo1_pos);
+    delay(servo_speed);
   }
-  servo1.write(servo1Val);
-  servo2.write(servo2Val);
-  delay(servoSpeed);
+
+  if (kpd.getKeys()) {
+    for (int i=0; i<LIST_MAX; i++) {
+      if (kpd.key[i].stateChanged) {
+        switch(kpd.key[i].kstate){
+          case RELEASED:
+            switch(kpd.key[i].kchar){
+              case 'D':
+                servos_moving[0][0] = 0;
+                break;
+              case 'C':
+                servos_moving[0][0] = 0;
+                break;
+              case '#':
+                servos_moving[1][0] = 0;
+                break;
+              case '9':
+                servos_moving[1][0] = 0;
+                break;
+            }
+            break;
+          case PRESSED:
+            switch(kpd.key[i].kchar) {
+              case 'D':
+                servos_moving[0][0] = 1;
+                break;
+              case 'C':
+                servos_moving[0][0] = -1;
+                break;
+              case '#':
+                servos_moving[1][0] = 1;
+                break;
+              case '9':
+                servos_moving[1][0] = -1;
+                break;
+            }
+            break;
+        }
+      }
+    }
+  }
+  
 }
+
+
 
